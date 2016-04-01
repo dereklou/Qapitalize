b5058dff4e0c5c69c61857baaefbbb2a24bbc95d
Naseem Alnaji <nalnaji@berkeley.edu>
2016-04-01 00:25:18 -0700

    basic setup

---
+++ src/containerFactory.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let path       = require('path');
+let dependable = require('dependable');
+
+module.exports = {
+  createContainer
+};
+
+function createContainer() {
+  let container = dependable.container();
+  let entries   = [
+    'app.js',
+    'controllers'
+  ];
+
+  // load each entry as a module or a directory
+  // with a list of modules inside without recursion
+  entries.forEach(entry => {
+    container.load(path.join(__dirname, entry));
+  });
+
+  return container;
+}
